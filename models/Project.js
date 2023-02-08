2e5b8863c88cc18be2f28beb2e0a72b870ee0069
prasunpersonal <prasunsarkar.personal@gmail.com>
2023-02-09 01:43:16 +0530

    update

--- models/Project.js
+++ models/Project.js
@@ -3,10 +3,13 @@ import mongoose from "mongoose";
 const Project = mongoose.model('Project', mongoose.Schema({
     userId: {type: mongoose.Schema.Types.ObjectId, required: true},
     paragraph: {type: Array, required: true},
-    tags: {type: Object, required: true},
-    appearances: {type: Array, required: true},
+    namedEntityTags: {type: Object, required: true},
+    eventEntityTags: {type: Object, required: true},
+    namedEntityAppearances: {type: Array, required: true},
+    eventEntityAppearances: {type: Array, required: true},
     filename: {type: String, required: true},
-    words: {type: Object, required: false, default: {}},
+    namedEntities: {type: Object, required: false, default: {}},
+    eventEntities: {type: Object, required: false, default: {}},
     relations: {type: Object, required: false, default: {}},
 }, {minimize: false}));
 
